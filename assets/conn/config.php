9f8236f99c721379860637ae1600e2123ca8af1a
dimyati000 <email@dimyati000.com>
2024-10-19 10:33:14 +0700

    upadate

--- assets/conn/config.php
+++ assets/conn/config.php
@@ -1,7 +1,7 @@
 <?php 
 $host = "localhost";
 $user = "root";
-$pass = "";
+$pass = "root";
 $db = "cf_pakar";
 $conn = mysqli_connect($host, $user, $pass, $db)or die("Tidak terkoneksi ke server")
 ?>
